81ab6f4102b7902f2c0dd423a6cc65dcc13d9b28
Александр Волосников <silviroshcnv@gmail.com>
2021-03-29 01:21:06 +0300

    добавил метод для получения информации о VIN и поправил метод для проверки

--- TraficPoliceLib/VIN_Class.cs
+++ TraficPoliceLib/VIN_Class.cs
@@ -34,10 +34,61 @@ namespace TraficPoliceLib
             if (country == null)
                 return false;
 
+            if (!char.IsDigit(_vin[13]) || !char.IsDigit(_vin[14]) ||
+                !char.IsDigit(_vin[15]) || !char.IsDigit(_vin[16]))
+                return false;
+
             if (_vin[8].ToString() != CalculatingChecksum(_vin))
                 return false;
 
+
             return true;
+        }
+
+        public static string VinInfo(string vin)
+        {
+            _vin = vin.ToUpper();
+
+            string result = $"Информация о указанном VIN ({vin}):\n";
+
+            if (_vin.Length != 17)
+                return result += "Ошибка!\nДлина VIN должна составлять 17 знаков";
+
+            foreach (var @char in _vin)
+            {
+                if (!_allowedChars.Contains(@char))
+                    return result += "Ошибка!\nVIN состоит из запрещённых символов";
+            }
+
+            if (_vin[0] == '0')
+                return result += "Ошибка!\nПервый символ не может быть равен 0";
+
+            Country country = CountriesList.FirstOrDefault(i => i.GeoAreaCode == _vin[0]
+            && i.IsActive == true && ExpandedCode(i.CodeInterval).Contains(_vin[1].ToString()));
+
+            if (country == null)
+                return result += "Ошибка!\nНеверный всемирный индекс изготовителя (WMI)";
+            else if (!country.IsActive)
+                return result += $"Ошибка!\nСимвол '{_vin[1]}' не используется для какой либо страны в регионе \"{country.GeoArea.Name}\"";
+
+            if (ModelYearDict[_vin[9]] == 0)
+                return "Ошибка!\nНеверный модельный год";
+
+            if (!char.IsDigit(_vin[13]) || !char.IsDigit(_vin[14]) ||
+                !char.IsDigit(_vin[15]) || !char.IsDigit(_vin[16]))
+                return "Ошибка!\nПоследние 4 знака обязательно должны быть цифрами";
+
+            if (_vin[8].ToString() != CalculatingChecksum(_vin))
+                return result += "Ошибка!\nНеверный контрольный символ";
+
+            result += $"Регион: {country.GeoArea.Name}\nСтрана сборки: {country.Name}\n" +
+                $"Фирма-производитель: неизвестно\nКолесная база: неизвестно\n" +
+                $"Тип кузова: неизвестно\nДвигатель: неизвестно\n" +
+                $"Тип трансмиссии: неизвестно\nГод выпуска: {ModelYearDict[_vin[9]]}\n" +
+                $"Отделение завода: неизвестно\nПорядковый номер: неизвестно\n" +
+                $"Контрольный символ: успешно прошёл проверку";
+
+            return result;
         }
 
         #region Methods with calculations
@@ -79,7 +130,7 @@ namespace TraficPoliceLib
                 var item = vin[i];
                 if (i != 8)
                 {
-                    if (Char.IsDigit(item))
+                    if (char.IsDigit(item))
                         digitalEquivalent += item;
                     else
                         digitalEquivalent += DigitalEquivalentDict[item];
@@ -90,7 +141,7 @@ namespace TraficPoliceLib
             {
                 var item = digitalEquivalent[i];
 
-                sum += (int)Char.GetNumericValue(item) * WeightOfCharactersDict[i + 1];
+                sum += (int)char.GetNumericValue(item) * WeightOfCharactersDict[i + 1];
             }
 
             int a = (int)(Math.Floor((double)sum / 11) * 11);
